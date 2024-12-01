a25d4138773b8fd64ce65f9257b5cea3577475f7
A H M Golam Hafiz <hasnathafiz229@gmail.com>
2024-11-30 22:22:53 -0600

    pipe added and class transformer & validator applied

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,39 +1,59 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  ValidationPipe,
+} from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
 import { ConfigService } from 'src/config/config.service';
+import { IsPositivePipe } from 'src/pipes/is-positive.pipe';
+import { CreateEpisodeDto } from 'src/dto/create-episode.dto';
 
 @Controller('episodes')
 export class EpisodesController {
-    constructor(
-        private episodesService: EpisodesService,
-        private configService: ConfigService
-    ) {}
+  constructor(
+    private episodesService: EpisodesService,
+    private configService: ConfigService,
+  ) {}
 
-    @Get()
-    findAll(@Query("sort") sort: "asc" | "desc" = "desc") {
-        console.log(sort)
-        return this.episodesService.findAll(sort)
+  @Get()
+  findAll(
+    @Query('sort') sort: 'asc' | 'desc' = 'desc',
+    @Query('limit', new DefaultValuePipe(100), ParseIntPipe, IsPositivePipe) limit: number,
+  ) {
+    console.log(sort);
+    return this.episodesService.findAll(sort);
+  }
+
+  @Get('featured')
+  findFeatured() {
+    return this.episodesService.findFeatured();
+  }
+
+  @Get(':id')
+  async findOne(@Param() id: string) {
+    console.log(id);
+    const episode = await this.episodesService.findOne(id);
+    if (!episode) {
+      throw new NotFoundException('Episode not found');
+      // throw new HttpException("Episode not found", HttpStatus.NOT_FOUND);
+      // throw new Error("Episode not found");
     }
 
-    @Get("featured")
-    findFeatured() {
-        return this.episodesService.findFeatured()
-    }
+    return episode;
+  }
 
-    @Get(":id")
-    async findOne(@Param() id: string) {
-        console.log(id)
-        const episode = await this.episodesService.findOne(id);
-        if(!episode) {
-            throw new Error("Episode not found");
-        }
-
-        return episode;
-    }
-
-    @Post()
-    create(@Body() input: any) {
-        console.log(input)
-        return this.episodesService.create(input)
-    }
+  @Post()
+  create(@Body(ValidationPipe) input: CreateEpisodeDto) {
+    console.log(input);
+    return this.episodesService.create(input);
+  }
 }
