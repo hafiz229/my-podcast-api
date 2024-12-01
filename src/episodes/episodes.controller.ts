7f2f7a5beca5198b9cd708a57a78118a8dd36e99
A H M Golam Hafiz <hasnathafiz229@gmail.com>
2024-11-30 20:08:10 -0600

    applied dependency injection into the episodes controller and external config service used

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,27 +1,34 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { EpisodesService } from './episodes.service';
+import { ConfigService } from 'src/config/config.service';
 
 @Controller('episodes')
 export class EpisodesController {
+    constructor(
+        private episodesService: EpisodesService,
+        private configService: ConfigService
+    ) {}
+
     @Get()
     findAll(@Query("sort") sort: "asc" | "desc" = "desc") {
         console.log(sort)
-        return "all episodes"
+        return this.episodesService.findAll(sort)
     }
 
     @Get("featured")
     findFeatured() {
-        return "featured episodes"
+        return this.episodesService.findFeatured()
     }
 
     @Get(":id")
     findOne(@Param() id: string) {
         console.log(id)
-        return "one episode"
+        return this.episodesService.findOne(id)
     }
 
     @Post()
     create(@Body() input: any) {
         console.log(input)
-        return "new episodes"
+        return this.episodesService.create(input)
     }
 }
